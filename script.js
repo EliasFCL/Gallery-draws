d4a5dfa44d9533016f9f4311cccbe62006be566c
EliasFCL <eliascordero506@gmail.com>
2025-12-15 08:30:59 -0600

    Imágenes agregadas en cada carpeta

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll(".category-btn");
 const sections = document.querySelectorAll(".gallery-section");
 const backButtons = document.querySelectorAll(".back-btn");
 
-// Mostrar sección al hacer clic
+//Mostrar sección al hacer clic
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
         const target = btn.getAttribute("data-target");
@@ -13,29 +13,29 @@ buttons.forEach(btn => {
     });
 });
 
-// --- Abrir imagen en modal con fondo oscuro ---
+//Abrir imagen en modal con fondo oscuro 
 document.querySelectorAll(".gallery img").forEach(img => {
     img.addEventListener("click", () => {
 
-        // Crear overlay
+        //Crear overlay
         const overlay = document.createElement("div");
         overlay.className = "image-overlay";
 
-        // Imagen dentro del overlay
+        //Imagen dentro del overlay
         const bigImg = document.createElement("img");
         bigImg.src = img.src;
 
-        // Botón cerrar
+        //Botón cerrar
         const closeBtn = document.createElement("button");
         closeBtn.innerText = "Cerrar imagen";
         closeBtn.className = "close-overlay-btn";
 
-        // Cerrar al presionar el botón
+        //Cerrar al presionar el botón
         closeBtn.addEventListener("click", () => {
             overlay.remove();
         });
 
-        // Insertar elementos
+        //Insertar elementos
         overlay.appendChild(bigImg);
         overlay.appendChild(closeBtn);
 
